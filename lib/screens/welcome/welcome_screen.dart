ee403bd3d320341d289f61514c83940a1f8236e3
matheus2_andrade <matheus.moura@nutrin.com.br>
2021-09-15 11:11:30 -0300

    first commit

---
+++ lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:websafe_svg/websafe_svg.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          WebsafeSvg.asset("assets/icons/bg.svg")
+        ],
+      )
+    );
+  }
+}
